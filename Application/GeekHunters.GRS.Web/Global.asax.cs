3ea26b8da2a20909d1542c65a5a03c238abf45b2
Kushan Fernando <info@kushan.me>
2017-08-13 01:25:53 +1200

    Autofac configured

--- Application/GeekHunters.GRS.Web/Global.asax.cs
+++ Application/GeekHunters.GRS.Web/Global.asax.cs
@@ -1,23 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using GeekHunters.GRS.BusinessModels;
+using GeekHunters.GRS.Services;
+using System;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
 
 namespace GeekHunters.GRS.Web
 {
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        public void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            this.ConfigAutofac();     
+        }
+
+        private void ConfigAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(typeof(BaseModel).Assembly);
+            builder.RegisterAssemblyTypes(typeof(BaseService).Assembly).AsImplementedInterfaces().SingleInstance();
+
+            builder.RegisterControllers(typeof(Global).Assembly);
+            builder.RegisterApiControllers(typeof(Global).Assembly);
+
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
         }
     }
 }
